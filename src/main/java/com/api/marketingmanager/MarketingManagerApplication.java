b8acbcb5b6ae46391461ea64920a93af9f9e1d55
Mikael Douglas <mikaeldouglas35@gmail.com>
2023-09-13 20:17:32 -0300

    first Commit

---
+++ src/main/java/com/api/marketingmanager/MarketingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.marketingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/marketingmanager/MarketingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.marketingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
